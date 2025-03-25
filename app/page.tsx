12f86bbdc41fab7f54df65a2722b68c3df43bb0d
gabrielfel1x <gabrielfelxx@gmail.com>
2025-03-25 13:56:19 -0300

    initial files

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="bg-secondary">
+      <h1 >teste</h1>
+      <h1 className="font-antarctican-mono">teste 2</h1>
+    </div>
+  );
+}
