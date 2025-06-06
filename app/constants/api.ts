d8794070177826221c4d6963064338e5977be168
Matheus Gois <mattheusgoiis@gmail.com>
2025-06-06 12:28:55 -0300

    migrate to env

--- app/constants/api.ts
+++ app/constants/api.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "http://127.0.0.1:3000";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
 export const API_ENDPOINTS = {
   SHOPS: "/shops/",
   LOGIN: "/login/",
